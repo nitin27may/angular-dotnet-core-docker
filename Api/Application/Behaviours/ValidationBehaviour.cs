909e82943cad9d32ab541cebee040c76cfeb8af3
Nitin Singh <nitin27may@gmail.com>
2022-12-11 19:32:04 -0500

    angular and .net upgrade (#8)
    Co-authored-by: Nitin Singh <nitin@Nitins-Laptop.local>

--- Api/Application/Behaviours/ValidationBehaviour.cs
+++ Api/Application/Behaviours/ValidationBehaviour.cs
@@ -17,7 +17,7 @@ public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TReques
         _validators = validators;
     }
 
-    public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
         if (_validators.Any())
         {
--- frontend/src/app/core/components/alert-messages/alert-messages.service.ts
+++ frontend/src/app/core/components/alert-messages/alert-messages.service.ts
@@ -17,7 +17,7 @@ export class AlertService {
           this.keepAfterNavigationChange = false;
         } else {
           // clear alert
-          this.subject.next();
+          this.subject.next(true);
         }
       }
     });
